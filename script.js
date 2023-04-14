753f39927b74b7ec4cefff6608630206049f6a37
Arka2394 <87031423+Arka2394@users.noreply.github.com>
2023-04-14 10:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	 return sampleObject.hasOwnProperty(key);
 }
 
+// Test the function
+console.log(hasKey("red")); // true
+console.log(hasKey("blue")); // false
+console.log(hasKey("white")); // true
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
